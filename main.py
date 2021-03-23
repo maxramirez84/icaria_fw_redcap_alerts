3c51d02be7db8aaa529ce20f00865354ba08d1a5
maxramirez84 <maxramirez84@gmail.com>
2021-03-23 12:44:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+""" Python script to setup alerts for ICARIA field workers. These alerts are for them to know if they have to do a
+household visit after AZi/Pbo administration or a Non-Compliant visit. In the context of the ICARIA Clinical Trial, a
+household visit is scheduled few days after the administration of the investigational product (azithromycin in this
+case). Moreover, if study participants are not coming to the scheduled study visits, another household visit will be
+scheduled to capture their status. This script is computing regularly which of the participants requires a household or
+Non-Compliant visit. This requirement is saved into an eCRF variable in the Screening DCI. This variable will be setup
+as part of the REDCap custom record label. Like this, field workers will see in a glance which participants they need to
+visit at their households."""
+
+__author__ = "Maximo Ramirez Robles"
+__copyright__ = "Copyright 2021, ISGlobal Maternal, Child and Reproductive Health"
+__credits__ = ["Maximo Ramirez Robles"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__date__ = "20210323"
+__maintainer__ = "Maximo Ramirez Robles"
+__email__ = "maximo.ramirez@isglobal.org"
+__status__ = "Dev"
