72c2f24311933eba90e951910a1da16b210551c7
andreubp <andreu.bp@gmail.com>
2024-08-14 12:47:24 +0200

    AVS

--- alerts.py
+++ alerts.py
@@ -657,7 +657,7 @@ def get_record_ids_new_ms(redcap_data, days_after_epi, excluded_epi_visits):
     
     
     A CANVIAR!!!!!! 
-    
+        
     ['int_date', 'a1m_date', 'hh_date', 'ms_date_contact', 'unsch_date','sae_hosp_admin_date','comp_date', 'ch_his_date', 'rtss_date',
      'rtss_vacc_rtss1_date', 'rtss_vacc_rtss2_date', 'rtss_vacc_rtss3_date','rtss_vacc_rtss4_date',
      'int_vacc_bcg_date','int_vacc_opv1_date','int_vacc_opv2_date','int_vacc_opv3_date',
@@ -730,13 +730,23 @@ def build_new_ms_alerts_df(redcap_data, record_ids, alert_string, event_names, l
     new_last_visit = pd.DataFrame(columns=['redcap_event_name'])
     for k, el in last_visit_dates.T.items():
         if k in record_ids:
-            last_visit = redcap_data.loc[k][
-                ['int_date', 'a1m_date', 'hh_date', 'sae_hosp_admin_date',
+            print(k)
+            last_visit = redcap_data.loc[k][['int_date', 'a1m_date', 'hh_date', 'ms_date_contact',
+                 'unsch_date', 'mig_date', 'sae_hosp_admin_date', 'rtss_date',
+                 'comp_date', 'ch_his_date', 'rtss_vacc_rtss1_date',
+                 'rtss_vacc_rtss2_date', 'rtss_vacc_rtss3_date',
+                 'rtss_vacc_rtss4_date']]
+            """
+              [['int_date', 'a1m_date', 'hh_date', 'sae_hosp_admin_date',
                  'ms_date_contact', 'unsch_date', 'mig_date', 'comp_date', 'ch_his_date',
                  'rtss_vacc_rtss1_date','rtss_vacc_rtss2_date','rtss_vacc_rtss3_date','rtss_vacc_rtss4_date']]
+                 
+            """
             last_visit = last_visit[last_visit.eq(el)]
             last_visit = last_visit[last_visit.notnull()]
+            print(last_visit)
             last_visit = last_visit.dropna(how='all')
+            print(last_visit)
             new_last_visit.loc[k] = last_visit.index[0]
     last_epi_visit = pandas.to_datetime(last_epi_visit)
     last_epi_visit = last_epi_visit.reset_index()
